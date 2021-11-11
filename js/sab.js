ff98db3db1ef3bbcb09c632002cc4601028368c5
Génesis Tobón <78097268+gntobonb@users.noreply.github.com>
2021-11-11 07:10:49 -0500

    prueba-oscuro

--- js/sab.js
+++ js/sab.js
@@ -1,4 +1,19 @@
-function myFunction() {
-  var element = document.body;
-  element.classList.toggle("dark-mode");
-}
+/*First part of function initiates the action when the button is pressed
+//Second part of the function initiates the action when the block is pressed as well
+
+
+//If getting by ID
+$("#control-uniqueID1").click(function() {
+  //if the control is clicked, we still have to make sure the button is submitted/clicked
+  $('#button-uniqueID1').click();
+});
+//Then if the button is clicked, run the thing
+$("#button-uniqueID1").click(function() {
+  $('#button-uniqueID1').toggleClass('box-active');
+  $('#control-uniqueID1').toggleClass('switch-active');
+});
+
+//*
+  function oscuro() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");}
