d808b78e5f0190648e359e744cbac528f9634d1f
Génesis Tobón <genesistobon@gmail.com>
2021-11-11 12:22:18 -0500

    modo-oscuro

--- js/sab.js
+++ js/sab.js
@@ -1,18 +1,17 @@
-//First part of function initiates the action when the button is pressed
-//Second part of the function initiates the action when the block is pressed as well
+function myFunction() {
+  let element = document.body;
+  element.classList.toggle('dark-mode');
+  
 
+  if(document.body.classList.contains('dark-mode')){
+    localStorage.setItem('dark', 'true');
+  } else {
+    localStorage.setItem('dark', 'false');
+  }
+};
 
-//If getting by ID
-$("#control-uniqueID1").click(function() {
-  //if the control is clicked, we still have to make sure the button is submitted/clicked
-  $('#button-uniqueID1').click();
-});
-//Then if the button is clicked, run the thing
-$("#button-uniqueID1").click(function() {
-  $('#button-uniqueID1').toggleClass('transform-btn-active');
-  $('#control-uniqueID1').toggleClass('transform-control-active');
-});
-
-function oscuro() {
-   var element = document.body;
-   element.classList.toggle("dark-mode");};
+  if(localStorage.getItem('dark') === 'true'){
+    document.body.classList.add('dark-mode');
+  } else {
+    document.body.classList.remove('dark-mode');
+  }
